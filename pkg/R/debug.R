bed94c418fb7e77192225bcce7f89816311be104
pcd (Peter Danenberg) <pcd@roxygen.org>
2011-06-22 02:58:32 +0000

    Some str options
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/debug@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/debug.R
+++ pkg/R/debug.R
@@ -3,12 +3,25 @@
 ##' Thanks, William Dunlap!
 ##' <https://stat.ethz.ch/pipermail/r-devel/2011-May/061098.html>
 ##' @param ... Objects to be debugged
+##' @param where Environment in which to evaluate the expressions
+##' @param strict.width \code{str}-option \code{strict.width} (\emph{quod vide})
+##' @param digits.d \code{str}-option \code{digits.d} (\emph{quod vide})
+##' @param vec.len \code{str}-option \code{vec.len} (\emph{quod vide})
+##' @param formatNum \code{str}-option \code{formatNum} (\emph{quod vide})
 ##' @return \code{NULL}
-debug <- function(..., where=parent.frame()) {
+debug <- function(...,
+                  where=parent.frame(),
+                  strict.width=strOptions()$strict.width,
+                  digits.d=strOptions()$digits.d,
+                  vec.len=strOptions()$vec.len,
+                  formatNum=strOptions()$formatNum) {
   promises <- as.list(substitute(list(...)))[-1]
-  ## str(structure(Map(Curry(eval, envir=where, enclos=where), promises),
   str(structure(Map(function(promise)
                     eval(promise, envir=where),
                     promises),
-                names=Map(deparse, promises)))
+                names=Map(deparse, promises)),
+      strict.width=strict.width,
+      digits.d=digits.d,
+      vec.len=vec.len,
+      formatNum=formatNum)
 }
